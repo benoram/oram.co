ef2f105277e3409307a040cbb92aacc9093e58de
Ben Oram <b@oram.co>
2020-10-06 15:08:10 -0600

    Added S3 buckets to handle redirects from www -> apex

---
+++ infrastructure/www-redirect-primary.tf
@@ -0,0 +1,41 @@
+resource "aws_s3_bucket" "www_redirect_primary" {
+  provider = aws.virginia
+  bucket = "${var.deploy_id}-www-redirect-primary"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }  
+
+  lifecycle_rule { 
+    id = "expire"
+    enabled = true
+    abort_incomplete_multipart_upload_days = 2
+    
+    expiration {
+      days = 0
+      expired_object_delete_marker = false
+    }
+  } 
+
+  versioning {
+    enabled = true
+  }
+
+  website {
+      redirect_all_requests_to = "https://${var.domain_name}"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "www_redirect_primary" {
+  provider = aws.virginia
+  bucket = aws_s3_bucket.www_redirect_primary.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
