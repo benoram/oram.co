30053359eeb2d62c64d10fd3243367ea7e82393f
Ben Oram <b@oram.co>
2020-10-07 06:58:20 -0600

    .

--- infrastructure/imports.tf
+++ infrastructure/imports.tf
@@ -1,7 +1,9 @@
 data "aws_ssm_parameter" "domain_name" {
+  provider = aws.oregon
   name = "/${var.deploy_id}/domain/name"
 }
 
 data "aws_route53_zone" "main" {
+  provider = aws.oregon
   name = data.aws_ssm_parameter.domain_name.value
 }
