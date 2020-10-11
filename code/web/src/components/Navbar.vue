b1b5a2507b751b9512bee6bdbe879449ca4e4b4e
Ben Oram <b@oram.co>
2020-10-11 13:42:37 -0600

    Add Keybase Link (#16)

--- code/web/src/components/Navbar.vue
+++ code/web/src/components/Navbar.vue
@@ -17,6 +17,10 @@
                             GitHub
                         </a>
 
+                        <a href="https://keybase.io/benoram" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            Keybase
+                        </a>
+
                         <a href="/documents/resume.pdf" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                             Resume
                         </a>
@@ -45,6 +49,10 @@
 
                 <a href="https://github.com/benoram" class="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out">
                     GitHub
+                </a>
+
+                <a href="https://keybase.io/benoram" class="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out">
+                    Keybase
                 </a>
 
                 <a href="@/assets/files/resume.pdf" class="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out">
